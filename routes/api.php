bddcc48b15c25fdbeb70552abca104212fd1344b
johirulalam <johirulalam365@gmail.com>
2022-01-03 20:07:12 +0600

    add use to get SellerController on api route and show all seller and single seller by their is

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\UserController;
 use App\Http\controllers\Buyer\BuyerController;
+use App\Http\controllers\Seller\SellerController;
 
 /*
 |--------------------------------------------------------------------------
